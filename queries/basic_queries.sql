b03c1be179319b8416040dc57711b0fe7a300927
Neena <137474831+NeenaThommy@users.noreply.github.com>
2025-12-07 06:29:53 +0530

    Update basic_queries.sql

--- queries/basic_queries.sql
+++ queries/basic_queries.sql
@@ -1,1 +1,10 @@
-
+-- List all customers from 'Bangalore'.
+SELECT 
+    customer_id,
+    first_name,
+    last_name,
+    email,
+    city,
+    join_date
+FROM customers
+WHERE lower(city) = 'bangalore';
