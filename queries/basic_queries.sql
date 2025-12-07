4623e90e93aa5ac17dea385a8894d1f7601bd491
Neena <137474831+NeenaThommy@users.noreply.github.com>
2025-12-07 06:37:08 +0530

    Update basic_queries.sql

--- queries/basic_queries.sql
+++ queries/basic_queries.sql
@@ -1,4 +1,4 @@
--- List all customers from 'Bangalore'.
+-- 1) List all customers from 'Bangalore'.
 SELECT 
     customer_id,
     first_name,
@@ -8,3 +8,12 @@ SELECT
     join_date
 FROM customers
 WHERE lower(city) = 'bangalore';
+
+--2) Find all orders placed in the last 30 days.
+select 
+    order_id,
+	customer_id,
+    order_date,
+    total_amount
+from orders
+where order_date >= current_date - interval '30 days'
